01a447c42a512f746b36c72197ce8e7b10ee46aa
rdutto <rdutto@kafesistemas.com.ar>
2021-04-18 22:17:18 -0300

    FEAT user crud rest API

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,16 @@
 import express from 'express';
+import userController from '../controllers/users.js'
 
 const router = express.Router();
+
+router.get('/', userController.getAllUsers)
+
+router.get('/:id', userController.getUserById)
+
+router.post('/', userController.createUser)
+
+router.delete('/:id', userController.deleteUserById)
+
+router.patch('/:id', userController.updateUserById)
+
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import usersRoutes from './routes/users.js'
 
 dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/users', usersRoutes);
 
 app.listen(
     process.env.PORT, 
